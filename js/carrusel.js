0b466b00cb512e77723d8bbcfe93fb8e93d8abd7
Elias <elias.d.gonzalez25@gmail.com>
2024-02-14 16:01:00 -0300

    Sobre mi

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,20 +1,30 @@
 //CARRUCEL
-const productContainers=[...document.querySelectorAll('.project-container')];
-const nxtBtn=[...document.querySelectorAll('.nxt-btn')];
-const preBtn=[...document.querySelectorAll('.pre-btn')];
+const productContainers = [...document.querySelectorAll('.project-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
 
-productContainers.forEach((item,i)=>{
+productContainers.forEach((item, i) => {
   let containerDimensions = item.getBoundingClientRect();
   let containerwidth = containerDimensions.width;
+  let isAnimating = false;
 
-  nxtBtn[i].addEventListener('click',()=>{
-    item.scrollLeft += containerwidth;
+  nxtBtn[i].addEventListener('click', () => {
+    if (!isAnimating) {
+      isAnimating = true;
+      item.scrollLeft += containerwidth;
+      setTimeout(() => {
+        isAnimating = false;
+      }, 500); // Ajusta este valor según la duración de tu animación
+    }
+  });
 
-  })
-  preBtn[i].addEventListener('click',()=>{
-    item.scrollLeft -= containerwidth;
-   
-  })
-
-
-})
+  preBtn[i].addEventListener('click', () => {
+    if (!isAnimating) {
+      isAnimating = true;
+      item.scrollLeft -= containerwidth;
+      setTimeout(() => {
+        isAnimating = false;
+      }, 500); // Ajusta este valor según la duración de tu animación
+    }
+  });
+});
