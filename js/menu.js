5e866b2511bec0f830e9af430bd66189cfb7eda6
Elias <elias.d.gonzalez25@gmail.com>
2024-02-23 18:15:48 -0300

    reponsive en proceso

---
+++ js/menu.js
@@ -0,0 +1,30 @@
+let menu = document.getElementById("menu-hamburger");
+let navbar = document.getElementById("navbar");
+let header = document.getElementById("header-bar");
+let mediaQuery = window.matchMedia('(min-width: 767px)');
+
+let isActive = false;
+
+function menuClick() { 
+    menu.addEventListener('click', () => {
+        if (!isActive) {                 
+          navbar.style.display = "block";
+          header.style.height ="25vh"  
+          isActive = true;      
+                             
+        } else {
+            navbar.style.removeProperty('display')
+            header.style.removeProperty('height')         
+              isActive = false;           
+        }
+    });
+}
+window.addEventListener('resize', () => {
+  if (isActive && !mediaQuery.matches) {
+     
+      navbar.style.removeProperty('display');
+      header.style.removeProperty('height');
+      isActive = false;
+  }
+});
+menuClick();
