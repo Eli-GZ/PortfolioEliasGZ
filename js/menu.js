0ff39b8d8bee82f364a040e66d0c2211fcc41248
Elias <elias.d.gonzalez25@gmail.com>
2024-02-27 11:30:56 -0300

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,36 @@
 let menu = document.getElementById("menu-hamburger");
 let navbar = document.getElementById("navbar");
 let header = document.getElementById("header-bar");
+let clickMenu = document.querySelector("ul")
 let mediaQuery = window.matchMedia('(min-width: 767px)');
 
 let isActive = false;
 
-function menuClick() { 
+function hambClick() { 
     menu.addEventListener('click', () => {
         if (!isActive) {                 
           navbar.style.display = "block";
           header.style.height ="30vh"  
-          isActive = true;      
-                             
+          isActive = true;                   
         } else {
             navbar.style.removeProperty('display')
             header.style.removeProperty('height')         
               isActive = false;           
-        }
+        }        
     });
 }
+function menuClick() { 
+   clickMenu.addEventListener('click', () => {
+    if (!isActive) {                
+      navbar.style.removeProperty('display')
+      header.style.removeProperty('height')  
+      isActive = true;                   
+    } else {               
+          isActive = false;           
+    }   
+ });
+}
+
 window.addEventListener('resize', () => {
   if (isActive && !mediaQuery.matches) {
      
@@ -27,4 +39,5 @@ window.addEventListener('resize', () => {
       isActive = false;
   }
 });
+hambClick();
 menuClick();
